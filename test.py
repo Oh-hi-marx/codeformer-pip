6e1679571a7e005c2e5246d2541c1e1288dd9254
Oh-hi-marx <40456769+Oh-hi-marx@users.noreply.github.com>
2024-03-16 02:52:34 +1300

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,15 @@
-import cv2 
-from codeformer.app import inference_app
+if __name__ == "__main__":
+    import cv2 
+    from codeformer.app import inference_app
 
-img = cv2.imread("test.png")
-img = inference_app(
-  image=img,
-  background_enhance=True,
-  face_upsample=True,
-  upscale=2,
-  codeformer_fidelity=0.5,
-   min_size= 50, 
-   max_size= 150
-)
-cv2.imwrite("out.png", img)
+    img = cv2.imread("test.png")
+    img = inference_app(
+      image=img,
+      background_enhance=True,
+      face_upsample=True,
+      upscale=2,
+      codeformer_fidelity=0.5,
+       min_size= 50, 
+       max_size= 150
+    )
+    cv2.imwrite("out.png", img)
